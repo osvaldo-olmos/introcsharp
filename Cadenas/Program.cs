dcae1b0e8b8d38c91131ae84a603075173274435
osvaldo-olmos <osvaldo.olmos@gmail.com>
2018-07-09 20:27:59 -0300

    New example of string usage added

---
+++ Cadenas/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Cadenas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string cadena1 = "John Wayne";
+            string cadena2 = String.Copy(cadena1);
+
+            // == Compara por valor
+            Console.WriteLine(cadena1==cadena2);
+
+            //Comparacion por referencia
+            Console.WriteLine(Object.ReferenceEquals(cadena1, cadena2));
+
+            //Casteo
+            Console.WriteLine( (object) cadena1 == (object) cadena2);
+
+            //Dos cadenas iguales se almacenan en el mismo espacio de memoria
+            string cadena3 = "Matt Dammon";
+            string cadena4 = "Matt Dammon";
+
+            Console.WriteLine( (object) cadena3 == (object) cadena4);
+        }
+    }
+}
