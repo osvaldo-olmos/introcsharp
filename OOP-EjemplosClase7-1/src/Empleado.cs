a02668975d0c0c017606c325ad6a481ade71e4da
osvaldo-olmos <osvaldo.olmos@gmail.com>
2018-07-17 09:15:21 -0300

    New examples of Object methods overriding added

--- OOP-EjemplosClase7-1/src/Empleado.cs
+++ OOP-EjemplosClase7-1/src/Empleado.cs
@@ -14,6 +14,35 @@ namespace empleados
         }
 
         public abstract double SueldoMensual();
+
+        public override bool Equals(object obj)
+        {
+            //
+            // See the full list of guidelines at
+            //   http://go.microsoft.com/fwlink/?LinkID=85237
+            // and also the guidance for operator== at
+            //   http://go.microsoft.com/fwlink/?LinkId=85238
+            //
+            
+            if (obj == null || GetType() != obj.GetType())
+            {
+                return false;
+            }
+            
+            // TODO: write your implementation of Equals() here
+            return (obj is Empleado) && this.DNI == (obj as Empleado).DNI;
+        }
+        
+        // override object.GetHashCode
+        public override int GetHashCode()
+        {
+            return this.DNI;
+        }
+
+        public override string ToString()
+        {
+            return $"{DNI} - {Apellido}, {Nombre}";
+        }
     }
 
 }
--- OOP-EjemplosClase7-1/src/Program.cs
+++ OOP-EjemplosClase7-1/src/Program.cs
@@ -8,7 +8,7 @@ namespace empleados
         static void Main(string[] args)
         {
             EmpleadoFulltime empleado1 =new EmpleadoFulltime(123456, "Osvaldo", "Olmos", 100.34);
-            EmpleadoFulltime empleado2 =new EmpleadoFulltime(999999, "Mario", "Mandzucick", 100000.50);
+            EmpleadoFulltime empleado2 =new EmpleadoFulltime(123457, "Mario", "Mandzucick", 100000.50);
             Freelancer empleado3 = new Freelancer(1111111, "Luka", "Modric", 20.50, 100);
 
             List<Empleado> empleados =new List<Empleado>();
@@ -18,8 +18,11 @@ namespace empleados
 
             foreach (Empleado empleado in empleados)
             {
-                Console.WriteLine($"El empleado: {empleado.Apellido} cobra: {empleado.SueldoMensual()}");
+                Console.WriteLine($"El empleado: {empleado.ToString()} cobra: {empleado.SueldoMensual()}");
             }
+
+            Console.WriteLine($"{empleado1.Equals(empleado2)}");
+            Console.WriteLine($"{Object.ReferenceEquals(empleado1, empleado2)}");
         }
     }
 }
