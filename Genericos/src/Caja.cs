42e807c29332ca4f09844101dc4d1adefa3a88c0
osvaldo-olmos <osvaldo.olmos@gmail.com>
2018-09-02 23:47:57 -0300

    Generic Types example added

---
+++ Genericos/src/Caja.cs
@@ -0,0 +1,25 @@
+using System;
+
+namespace Genericos.src
+{
+    public class CajaLimitada<T> where T : struct
+    {
+
+    }
+
+    public class CajaLimitada<X0, X1>
+        where X0 : struct
+        where X1 : IEquatable<X1>
+    {
+
+    }
+
+    public class CajaLimitada<T, U, V>
+        where T : struct
+        where U : Array<U>
+        where V : new()
+    {
+
+    }
+
+}
---
+++ Genericos/src/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Genericos
+{
+    public class Caja<T>
+    {
+        public T Contenido { get; private set; }
+
+        public Caja(T contenido)
+        {
+            Contenido = contenido;
+        }
+    }
+
+    public class CajaCompleja<T, Content1, Content2>
+    {
+        public Content1 C1 { get; set; }
+        public Content2 C2 { get; set; }
+        public T Item { get; set; }
+
+        public CajaCompleja(Content1 c1, Content2 c2)
+        {
+            C1 = c1;
+            C2 = c2;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Caja<int> cajaDeEntero = new Caja<int>(50);
+            Caja<string> cajaDeString = new Caja<string>("contenido");
+
+            Console.WriteLine(cajaDeEntero.Contenido);
+            Console.WriteLine(cajaDeString.Contenido);
+
+            //Anidamiento
+            var cajaDeCajas = new Caja<Caja<string>>(cajaDeString);
+            Console.WriteLine(cajaDeCajas.Contenido.Contenido);
+
+            //Mas de un tipo generico
+            var cajaCompleja = new CajaCompleja<double, decimal, float>(1, 2);
+            cajaCompleja.Item = 3;
+        }
+    }
+}
