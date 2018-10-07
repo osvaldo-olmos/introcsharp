6700f71fd8d65236ec555c64ec28de24c6e06bec
osvaldo-olmos <osvaldo.olmos@gmail.com>
2018-10-07 15:56:26 -0300

    New examples for anonymous types added

---
+++ TiposAnonimos/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace TiposAnonimos
+{
+
+    class Persona
+    {
+        public string Nombre {get; set; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Persona persona = new Persona(){ Nombre ="Hector"};
+
+            //Objetos de tipos anonimos
+            var persona1 = new { Name = "Roberto" };
+            var persona2 = new { Nombre = "Pedrito", Edad = "15" };
+            var persona3 = new { Edad = "15", Nombre = "Pedrito" };
+            var persona4 = new { Nombre = "Pedrito", Edad = "15" };
+
+            // No es tipado dinamico
+            //persona1.Edad = "12";
+
+            Console.WriteLine(persona2.ToString());
+            Console.WriteLine(persona2.Equals(persona3));
+            Console.WriteLine(persona2.Equals(persona4));
+        }
+    }
+}
