33aa3ecefe2fdd224ac3944e8a336cdb6b4495ce
osvaldo-olmos <osvaldo.olmos@gmail.com>
2018-11-12 22:05:57 -0300

    Managed and unmanaged resources example added

---
+++ Using/src/Coche.cs
@@ -0,0 +1,11 @@
+namespace Using.src
+{
+    class Coche
+    {
+        ~Coche()
+        {
+            // Liberacion de recursos aca
+        }
+    }
+
+}
--- Using/src/Descartable.cs
+++ Using/src/Descartable.cs
@@ -2,18 +2,56 @@ using System;
 
 public class Descartable : IDisposable
 {
+    bool is_disposed = false;
+    private IDisposable databaseConnection;
+    private IDisposable frameBufferImage;
+
+    protected virtual void Dispose(bool disposing)
+    {
+        if (!is_disposed) // Solo si no esta Disposed
+        {
+            if (disposing)
+            {
+                Console.WriteLine("Liberando recursos manejados");
+                //Ejemplos
+                if (this.databaseConnection != null)
+                {
+                    this.databaseConnection.Dispose();
+                    this.databaseConnection = null;
+                }
+                if (this.frameBufferImage != null)
+                {
+                    this.frameBufferImage.Dispose();
+                    this.frameBufferImage = null;
+                }
+            }
+            Console.WriteLine("Liberando recursos no manejados");
+            //Ejemplo
+            //Win32.DestroyHandle(this.CursorFileBitmapIconServiceHandle);
+        }
+        this.is_disposed = true;
+    }
+
     public Descartable()
     {
         Console.WriteLine("Nuevo Descartable.");
     }
+
     public void Dispose()
     {
-        Console.WriteLine("Dispose de Descartable ejecutado.");
+        Dispose(true);
+        // Le aviso al GC que no lo finalice
+        GC.SuppressFinalize(this);
+    }
 
+    ~Descartable()
+    {
+        Dispose(false);
+        Console.WriteLine("En el destructor.");
     }
 
     public void DoSomething()
     {
-        Console.WriteLine("Descartable Doing Something.");;
+        Console.WriteLine("Descartable Doing Something."); ;
     }
 }
