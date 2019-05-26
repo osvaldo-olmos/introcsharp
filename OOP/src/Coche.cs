e84d07c1b0654ba9e7f69b3bfcea1cd7df5e000e
osvaldo-olmos <osvaldo.olmos@gmail.com>
2019-05-26 15:30:58 -0300

    rename some fields and arguments

--- OOP/src/Coche.cs
+++ OOP/src/Coche.cs
@@ -4,47 +4,47 @@ namespace OOP
 {
     class Coche
     {
-            protected double velocidad=0;
-            public double Velocidad
-            {
+        protected double velocidad = 0;
+        public double Velocidad
+        {
             get
-                {
-                    return velocidad;
-                }
+            {
+                return velocidad;
             }
+        }
 
-            public string Marca;
-            public string Modelo;
-            public string Color;
-            public string Patente;
+        public string marca;
+        public string modelo;
+        public string color;
+        public string patente;
 
         public Coche(string marca, string modelo, string color, string patente)
         {
-                Marca =marca;
-                Modelo =modelo;
-                Color =color;
-                Patente =patente;
+            this.marca = marca;
+            this.modelo = modelo;
+            this.color = color;
+            this.patente = patente;
         }
-    
-        public virtual void Acelerar(double cantidad)
+
+        public virtual void Acelerar(double deltaVelocidad)
         {
-        // Aquí se le dice al motor que aumente las revoluciones pertinentes, y...
+            // Aquí se le dice al motor que aumente las revoluciones pertinentes, y...
             Console.WriteLine("Accionando el mecanismo de aceleración por defecto");
-            Console.WriteLine("Incrementando la velocidad en {0} km/h", cantidad);
-        this.velocidad += cantidad;
+            Console.WriteLine("Incrementando la velocidad en {0} km/h", deltaVelocidad);
+            this.velocidad += deltaVelocidad;
         }
-    
-        public virtual void Girar(double cantidad)
+
+        public virtual void Girar(double grados, string direccion)
         {
             // Aquí iría el código para girar
-            Console.WriteLine("Girando el coche {0} grados", cantidad);
+            Console.WriteLine($"Girando el coche {grados} grados en direccion {direccion}");
         }
-    
-        public virtual void Frenar(double cantidad)
+
+        public virtual void Frenar(double deltaVelocidad)
         {
             // Aquí se le dice a los frenos que actúen, y...
-            Console.WriteLine("Reduciendo la velocidad en {0} km/h", cantidad);
-            this.velocidad -= cantidad;
+            Console.WriteLine("Reduciendo la velocidad en {0} km/h", deltaVelocidad);
+            this.velocidad -= deltaVelocidad;
         }
     }
 }
