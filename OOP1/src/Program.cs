f865e0601fe6fa0f40857b4b9631dd42d8b21861
Osvaldo Olmos <oolmos@xsidesolutions.com>
2019-06-09 23:36:12 -0300

    refactoring

---
+++ OOP1/src/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjemploClase6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Coche miCoche = new Coche("ABG2345J", "Volkswagen", "EAD555");
+            Coche miCoche = new CocheElectrico("ABG2345J", "Volkswagen", "EAD555");
+            miCoche.Arrancar();
+            miCoche.Acelerar(20);
+            miCoche.Consume(10);
+            (miCoche as CocheElectrico).Consume(3);
+            miCoche.Doblar(45);
+            miCoche.Frena();
+
+            Console.ReadKey();
+
+        }
+    }
+}
