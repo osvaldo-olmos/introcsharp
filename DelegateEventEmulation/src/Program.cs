33227b41278018f7a27bd011a4af4c50e7b7b422
Osvaldo Olmos <oolmos@xsidesolutions.com>
2019-08-13 15:30:30 -0300

    refactoring delegates as event handlers

--- DelegateEventEmulation/src/Program.cs
+++ DelegateEventEmulation/src/Program.cs
@@ -2,42 +2,6 @@
 
 namespace Delegados3
 {
-    public delegate void Del(string message);
-
-    class Publisher {
-
-        Del handler;
-        public void Subscribe(Del eventHandler){
-            this.handler += eventHandler;
-        }
-
-        public void UnSubscribe(Del eventHandler){
-            this.handler -= eventHandler;
-        }
-
-        public void Run(){
-
-            for(int i=0; i < 10; i++){
-                handler($"event {i} !!!");
-                System.Threading.Thread.Sleep(2000);
-            }
-            
-        }
-    }
-
-    class SubscriberOne {
-
-        public void EventHandler(string eventDetail){
-            Console.WriteLine($"{this} : Event handled - Event Detail: {eventDetail}");
-        }
-    }
-
-    class SubscriberTwo {
-        public void EventHandler(string eventDetail){
-            Console.WriteLine($"{this} : Epa! Recibi este evento: {eventDetail}");
-        }
-    }
-
     class Program
     {
         static void Main(string[] args)
