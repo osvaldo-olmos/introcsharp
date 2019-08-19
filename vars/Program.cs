b570ada34da771eb458c0b5b27c87527c11fb20a
Osvaldo Olmos <oolmos@xsidesolutions.com>
2019-08-19 20:29:25 -0300

    more var examples

--- vars/Program.cs
+++ vars/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace vars
@@ -10,8 +12,8 @@ namespace vars
         {
             var numeroElementos = 50;
             var otroNumero = 5L;
-            object objetito ="object";
-            objetito =78;
+            object objetito = "object";
+            objetito = 78;
             var nombre = "Pirulo";
             var x = nombre + numeroElementos; //string + int = string
             var xz = numeroElementos + nombre; //string + int = string
@@ -20,6 +22,38 @@ namespace vars
             var buenEstudiante = new Persona("Osvaldo");
 
             //var indeciso = null; //Error
+
+            ImprimoLista();
+            EjemploLoop();
+        }
+
+        private static void ImprimoLista()
+        {
+            var palabras = new List<string> { "hola", "hila", "hula" };
+            Console.WriteLine("LIST COUNT: " + palabras.Count);
+        }
+        private static void EjemploLoop()
+        {
+            int[] numbers = { 1, 2, 3, 4, 5 };
+
+            // Use a query expression to get all the odd numbers from the array.
+            // ... The var stores the result IEnumerable.
+            // var items = from item in numbers
+            //             where (item % 2 == 1)
+            //             select item;
+
+            var items = numbers.Where(x => x % 2 == 1);
+
+            // Each item is an int.
+            foreach (var item in items)
+            {
+                Console.WriteLine("VAR LOOP: {0}", item);
+            }
+            // Use the same loop but without var.
+            foreach (int item in items)
+            {
+                Console.WriteLine("LOOP 2: {0}", item);
+            }
         }
     }
 }
