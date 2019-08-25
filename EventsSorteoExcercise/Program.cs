0c18d5980b69cd07d9a189e9507701b870ddb72d
Osvaldo Olmos <oolmos@xsidesolutions.com>
2019-08-25 12:09:48 -0300

    Events excercise added

---
+++ EventsSorteoExcercise/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace EventsSorteoExcercise
+{
+/*
+ * Loteria: Es quien recibe las apuestas y realiza el sorteo. Una vez realizado el sorteo,
+ * informar el numero ganador a todos los apostadores.
+ *
+ *Apostador: Apuesta un número.
+ *
+ *Hacer un programa implemente este modelo, generando un numero random como el numero ganador.
+
+ */
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Loteria myLotery = new Loteria();
+            Apostador apostador1 =new Apostador();
+            myLotery.Apuestas.Add(5);
+            myLotery.ResultadoSorteo += apostador1.RecibirResultado;
+            Apostador apostador2 =new Apostador();
+            myLotery.Apuestas.Add(2);
+            myLotery.ResultadoSorteo += apostador2.RecibirResultado;
+            Apostador apostador3 =new Apostador();
+            myLotery.Apuestas.Add(4);
+            myLotery.ResultadoSorteo += apostador3.RecibirResultado;
+
+            myLotery.Sortear();
+        }
+    }
+}
