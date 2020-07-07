09115ca6d9ddc93a0279cae81e42120806a53491
osvaldo-olmos <osvaldo.olmos@gmail.com>
2020-07-07 10:53:30 -0300

    new example added

--- EvaluacionPerezosa/Program.cs
+++ EvaluacionPerezosa/Program.cs
@@ -8,12 +8,22 @@ namespace EvaluacionPerezosa
         {
 
             if(EvalLeft(false) && EvalRight(true)){
-                Console.WriteLine("Complete eval is true");
+                Console.WriteLine("&& eval is true");
             }
 
             if(EvalLeft(false) & EvalRight(true)){
-                Console.WriteLine("Complete eval is true");
+                Console.WriteLine("& eval is true");
             }
+
+            if(EvalLeft(true) || EvalRight(false)){
+                Console.WriteLine("|| eval is true");
+            }
+
+            if(EvalLeft(true) | EvalRight(false)){
+                Console.WriteLine("| eval is true");
+            }
+
+
 
         }
 
