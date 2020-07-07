829b9fd431bdfa3637926a9d0a59428b751a1ea6
osvaldo-olmos <osvaldo.olmos@gmail.com>
2020-07-07 10:35:41 -0300

    Evaluacion Perezosa added

---
+++ EvaluacionPerezosa/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace EvaluacionPerezosa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            if(EvalLeft(false) && EvalRight(true)){
+                Console.WriteLine("Complete eval is true");
+            }
+
+            if(EvalLeft(false) & EvalRight(true)){
+                Console.WriteLine("Complete eval is true");
+            }
+
+        }
+
+        private static bool EvalRight(bool value)
+        {
+            Console.WriteLine("Eval Right");
+            return value;;
+        }
+
+        private static bool EvalLeft(bool value)
+        {
+            Console.WriteLine("Eval Left");
+            return value;
+        }
+    }
+}
