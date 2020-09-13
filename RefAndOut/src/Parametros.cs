6ecbadc4f0cd671dd1ffc21327f78e8251f60ac5
osvaldo-olmos <osvaldo.olmos@gmail.com>
2020-09-13 20:28:27 -0300

    improve ref & out example

---
+++ RefAndOut/src/Parametros.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace RefAndOut
+{
+    public class Parametros
+    {
+
+
+        public void BuggySwap(int a, int b)
+        {
+            int m = a;
+            a = b;
+            b = m;
+            Console.WriteLine($"Inside BuggySwap --> a vale: {a} - b vale: {b}");
+        }
+
+
+        public void Swap(ref int a, ref int b)
+        {
+            int m = a;
+            a = b;
+            b = m;
+        }
+
+        public bool SumaMaxima(int a, int b, int max, out int resultado)
+        {
+            resultado = a + b;
+            if (resultado >= max)
+            {
+                resultado = max;
+                return true;
+            }
+            return false;
+        }
+
+        public string OojedMethod(ref int a, ref int b, out int s, out int r)
+        {
+            s = a + b;
+            r = a - b;
+            return a.ToString() + b.ToString();
+        }
+    }
+}
---
+++ RefAndOut/src/Program.cs
@@ -0,0 +1,32 @@
+using System;
+
+namespace RefAndOut.src
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 100, b = 5;
+            Console.WriteLine("Valor de a:" + a + " - Valor de b:" + b);
+
+            Parametros parametros = new Parametros();
+            parametros.BuggySwap(a, b);
+            
+            parametros.Swap(ref a, ref b);
+            Console.WriteLine("Valor de a:" + a + " - Valor de b:" + b);
+
+            int resSuma;
+            var isResultOk = parametros.SumaMaxima(a, b, 100, out resSuma);
+
+            if (isResultOk)
+                Console.WriteLine("La suma excedió el valor máximo");
+            else
+                Console.WriteLine("El resultado de la suma es " + resSuma);
+
+            int s, r;
+            parametros.OojedMethod(ref a, ref b, out s, out r);
+
+            Console.WriteLine("s es : " + s + " - r es : " + r);
+        }
+    }
+}
