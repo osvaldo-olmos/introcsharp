ce2c40f2e86f31a068997fecdfed82d8db40876e
osvaldo-olmos <osvaldo.olmos@gmail.com>
2020-09-15 21:30:12 -0300

    fix paths

--- RefAndOut/src/Program.cs
+++ RefAndOut/src/Program.cs
@@ -10,25 +10,25 @@ namespace RefAndOut.src
             Console.WriteLine("Valor de a:" + a + " - Valor de b:" + b);
 
             Parametros parametros = new Parametros();
-            parametros.BuggySwap(a, b);
+            //parametros.BuggySwap(a, b);
             
             parametros.Swap(ref a, ref b);
             Console.WriteLine("Valor de a:" + a + " - Valor de b:" + b);
 
-            int resSuma;
-            var isResultOk = parametros.SumaMaxima(a, b, 100, out resSuma);
+            // int resSuma;
+            // var isResultOk = parametros.SumaMaxima(a, b, 100, out resSuma);
 
-            if (isResultOk)
-                Console.WriteLine("La suma excedió el valor máximo");
-            else
-                Console.WriteLine("El resultado de la suma es " + resSuma);
+            // if (isResultOk)
+            //     Console.WriteLine("La suma excedió el valor máximo");
+            // else
+            //     Console.WriteLine("El resultado de la suma es " + resSuma);
 
-            int suma, resta;
-            string ret =parametros.OojedMethod(ref a, ref b, out suma, out resta);
+            // int suma, resta;
+            // string ret =parametros.OojedMethod(ref a, ref b, out suma, out resta);
 
-            Console.WriteLine($"{a} y {b} concatenados es: {ret}");
-            Console.WriteLine($"la suma de {a} + {b}  es : " + suma);
-            Console.WriteLine($"la resta de {a} - {b}  es : " + resta);
+            // Console.WriteLine($"{a} y {b} concatenados es: {ret}");
+            // Console.WriteLine($"la suma de {a} + {b}  es : " + suma);
+            // Console.WriteLine($"la resta de {a} - {b}  es : " + resta);
         }
     }
 }
