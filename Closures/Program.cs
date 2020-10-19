748edaf5402a2101d89ccbe6d521c5c1a544ea4a
Osvaldo Olmos <oolmos@xsidesolutions.com>
2020-10-19 14:25:10 -0300

    closure example added

---
+++ Closures/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lambdas
+{
+
+    class Program
+    {
+
+        private static void RunEjemplo1()
+        {
+            List<Action> actions = new List<Action>();
+
+            //Add some actions
+            for (int counter = 0; counter < 10; counter++)
+            {
+                actions.Add(() => Console.WriteLine($"My number is {counter}"));
+            }
+
+            //Call the actions. Which numbers will be printed out ?
+            foreach (var action in actions)
+            {
+                action();
+            }
+        }
+
+        private static void ClosureExample()
+        {
+            var myVar = "this is good";
+
+            Func<string, string> myFunc = delegate (string var1)
+                                             {
+                                                 return var1 + myVar;
+                                             };
+
+            Console.WriteLine(myFunc("chamame "));
+        }
+
+
+        static void Main(string[] args)
+        {
+            //RunEjemplo1();
+            //ClosureExample();
+            ClosureExample1();
+        }
+
+        private static void ClosureExample1()
+        {
+            var myFunction = GetAFunc();
+            Console.WriteLine(myFunction(5));
+            Console.WriteLine(myFunction(6));
+        }
+
+        public static Func<int, int> GetAFunc()
+        {
+            var myVar = 1;
+            Func<int, int> myMethod = delegate (int param1)
+                                    {
+                                        myVar = myVar + 1;
+                                        return param1 + myVar;
+                                    };
+            return myMethod;
+        }
+    }
+}
